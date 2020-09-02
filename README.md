bc9e27c116e364c6fcdf5110d21b5745482bcb1a
tycalwayslove <1186446929@qq.com>
2020-09-02 08:58:55 +0800

    ci(按需加载和less的相关配置): 开发配置

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 2. 添加 eslint+husky+prettier+lint-staged 来统一前端代码风格
 3. 使用 react-app-rewired + customize-cra 来修改webpack配置
 3. 界面 ui(antd),配置式路由(react-router v4),全局状态管理(react-redux)
+3.1 界面布局遇到的问题
+    - 引入less
+        1. npm run eject 暴露配置添加less-loader
+        2. customize-cra 中的addLessLoader方法
+            > 不同less-loader版本写法不一致
+    - antd 的按需加载
+    -
--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,37 @@
-const { override, addWebpackAlias } = require("customize-cra");
+const {
+  override,
+  addWebpackAlias,
+  addLessLoader,
+  fixBabelImports,
+} = require("customize-cra");
 
 const path = require("path");
 
 module.exports = override(
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "lib",
+    style: "css",
+  }),
+
+  /**
+   * https://github.com/arackaf/customize-cra/blob/master/api.md#addlessloaderloaderoptions
+   * 前往customize-cra 查看更多配置信息
+   * addLessLoader less不同版本配置的方式不同
+   * 具体查阅 https://github.com/ant-design/ant-design/issues/23624
+   */
+  addLessLoader({
+    //less-loader@5
+    // javascriptEnabled: true,
+    // modifyVars: {
+    //   "@primary-color": "#1DA57A",
+    // },
+    // less-loader@>6
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#1DA57A" },
+    },
+  }),
   addWebpackAlias({
     ["@"]: path.resolve(__dirname, "src"),
   })
